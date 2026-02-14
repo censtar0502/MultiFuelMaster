360352cd7dedce99a9fa1768e32e1170c9d96708
Azizbek Khudayberdiev <censtar0502@gmail.com>
2026-02-14 11:25:37 +0500

    update 1

--- obj/Debug/net8.0-windows/MultiFuelMaster.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/MultiFuelMaster.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiFuelMaster")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05dcc3111569ada8fb109ff21c43ec81c33dcc20")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiFuelMaster")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiFuelMaster")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
